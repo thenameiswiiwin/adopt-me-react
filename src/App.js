a1ec5115ad1fc0afae4cbee551c9821cafdfaa7d
thenameiswiiwin <huyn.nguyen95@gmail.com>
2021-03-30 11:10:32 -0400

    React Router: details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./components/SearchParams";
+import Details from "./components/Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/details/:id">
+              {/* :id ==> is how you write a variables inside your Route. The variable will then get pass down as the property to the Details */}
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 };
---
+++ src/components/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>Details Route</h2>;
+};
+
+export default Details;
